668528ab5eb239aa12335bb83f6a12cfac3d8027
we25317735 <113696278+we25317735@users.noreply.github.com>
2025-08-04 10:11:10 +0800

    ...

--- utils/getSSRUser.js
+++ utils/getSSRUser.js
@@ -1,37 +1,29 @@
-import { cookies } from 'next/headers'
+import { cookies,headers  } from 'next/headers'
 import { parseJwt } from '@/services/user'
 import { redirect } from 'next/navigation'
 import { getUserById, userOrder, userCoupon } from '@/services/user'
 
-// SSR 取得當前使用者資料(沒有則跳回首頁)
+// 新版測試: SSR 取得當前使用者資料(沒有則跳回首頁)
 export async function getSSRUser() {
-  const accessToken = cookies().get('accessToken')
-  console.log('ssr cookie: ', accessToken)
+  const cookie = headers().get('cookie')
 
-  // 沒 token 就跳回首頁
-  if (!accessToken?.value) return redirect('/IGotBrew')
+  console.log("有沒有抓到 cookie", cookie);
+  const accessToken = cookie
+    ?.split('; ')
+    .find((c) => c.startsWith('accessToken='))
+    ?.split('=')[1]
 
-  const token = accessToken.value
+  if (!accessToken) return redirect('/IGotBrew')
 
-  let jwtUser
+  const jwtUser = parseJwt(accessToken)
+  if (!jwtUser?.id) return redirect('/IGotBrew')
 
-  try {
-    jwtUser = parseJwt(token)
-    if (!jwtUser?.id) throw new Error('Invalid token')
-  } catch {
-    return redirect('/IGotBrew')
-  }
+  const res = await getUserById(jwtUser.id, accessToken)
+  const user = res?.data?.data?.user
 
-  // 向後端拿資料
-  try {
-    const res = await getUserById(jwtUser.id, token)
-    const user = res?.data?.data?.user
+  if (!user) return redirect('/IGotBrew')
 
-    if (!user) throw new Error('User not found')
-    return user
-  } catch {
-    return redirect('/IGotBrew')
-  }
+  return user
 }
 
 // SSR 取得使用者 ID
